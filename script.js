9478396619836e0ee2f4172c284864009dd7bf5a
Suraj Goud <112563526+surajgoud36@users.noreply.github.com>
2026-09-24 14:08:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+Person.prototype.greet = function() {
+    console.log(
+        `Hello, my name is ${this.name}, I am ${this.age} years old.`
+    );
+};
+
+
+function Employee(name, age, jobTitle) {
+    Person.call(this, name, age);
+    this.jobTitle = jobTitle;
+}
+Object.setPrototypeOf(Employee.prototype,Person.prototype);
+
+Employee.prototype.jobGreet = function() {
+    console.log(
+        `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+    );
+};
+
+// Creating an instance of Person
+const person = new Person("Alice", 25);
+person.greet();
+// Expected Output: Hello, my name is Alice, I am 25 years old.
+
+// Creating an instance of Employee
+const employee = new Employee("Bob", 30, "Manager");
+employee.jobGreet();
+// Expected Output: Hello, my name is Bob, I am 30 years old, and my job title is Manager.
 
 // Do not change code below this line
 window.Person = Person;
